dee672d35447e10175b2f00c20307790e506fc15
med melek <medmelek007@gmail.com>
2019-12-14 07:09:27 -1200

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { myitem } from './mymodel';
+import { HeroService } from './hero.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tpfront';
+  mydata: myitem;
+  value: string ;
+
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    return this.heroService.getItem().subscribe(data => {
+      console.log(data);
+      this.mydata = data;
+      this.setValue();
+    });
+  }
+  setValue() {
+    this.value = this.mydata.title;
+  }
 }
